5a980f61ec899b50a70d9a35991ee944300356ca
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-04-04 09:59:39 -0400

    update data object with getValue, import helper functions, change render function, import axios, and update componentDidMount

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,27 +2,33 @@ import React, { Component } from "react";
 import classnames from "classnames";
 import Loading from "./Loading";
 import Panel from "./Panel";
-
+import axios from "axios";
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay
+ } from "helpers/selectors";
 const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6
+    getValue: getTotalInterviews
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm"
+    getValue: getLeastPopularTimeSlot
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday"
+    getValue: getMostPopularDay
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3"
+    getValue: getInterviewsPerDay
   }
 ];
 
@@ -40,7 +46,20 @@ class Dashboard extends Component {
   }
 
   componentDidMount() {
-    //code
+    this.setState({ loading: true });
+
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then(([days, appointments, interviewers]) => {
+      this.setState({
+        loading: false,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data
+      });
+    });
   }
 
   componentDidUpdate() {
@@ -70,7 +89,7 @@ class Dashboard extends Component {
         <Panel
           key={panel.id}
           label={panel.label}
-          value={panel.value}
+          value={panel.getValue(this.state)}
           onSelect={() => this.selectPanel(panel.id)}
         />
       ));
